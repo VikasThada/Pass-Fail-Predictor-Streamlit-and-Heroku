12da7377e8b7af2647474d1d5dbb763afdfb4d75
VikasThada <vikasthada@gmail.com>
2021-07-05 21:39:41 +0530

    Delete rungit.sh

--- rungit.sh
+++
@@ -1,7 +0,0 @@
-echo "# Pass-Fail-Predictor-Streamlit-and-Heroku" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/VikasThada/Pass-Fail-Predictor-Streamlit-and-Heroku.git
-git push -u origin main
